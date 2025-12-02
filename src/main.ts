5b424c410e28ee1c7b3d78682c1af4fd52efa511
MK-FS2 <mohamedkhttab67@gmail.com>
2025-12-02 21:52:21 +0200

    Feet: added create company

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { CustomException } from '@Shared/Filters';
 
 async function bootstrap() 
 {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+
+ 
+  app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true,transform:true}));
+
+  app.useGlobalFilters(new CustomException());
+
   const config = app.get(ConfigService)
   const port = Number(config.get("port"))
 
