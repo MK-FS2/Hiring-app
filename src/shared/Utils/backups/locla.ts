5d075033d9a59d0d26b3d26ff18748fd1fcb94b4
MK-FS2 <mohamedkhttab67@gmail.com>
2026-01-23 17:26:47 +0200

    Deploy6

---
+++ src/shared/Utils/backups/locla.ts
@@ -0,0 +1,34 @@
+// /* eslint-disable @typescript-eslint/no-floating-promises */
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ConfigService } from '@nestjs/config';
+// import { ValidationPipe } from '@nestjs/common';
+// import { CustomException } from '@Shared/Filters';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   app.useGlobalFilters(new CustomException());
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: false,
+//   });
+
+//   const config = app.get(ConfigService);
+//   const port = Number(process.env.PORT) || Number(config.get('port')) || 5000;
+
+//   await app.listen(port, '0.0.0.0');
+//   console.log(`Server is running on port ${port}`);
+// }
+
+// bootstrap();
